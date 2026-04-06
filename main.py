ffe761f23f1e507b3b9fa0fbe913d43fe074dcc7
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-06 14:02:03 -0400

    Création de plusieurs objets voiture et appelle à la fonction 'ajouter_voiture'

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from voiture import Voiture
+from crud_db import ajouter_voiture,connecter_db
 
 v1 = Voiture("Volkswagen", "Jetta", 2024, 29478)
 v1.afficher_voiture()
-
-from crud_db import connecter_db
 
 connexion = None
 
@@ -15,3 +14,12 @@ except Exception as e:
 finally:
     if 'connexion' in locals():
         connexion.close()
+
+v2=Voiture("Toyota", "Corolla", 2022, 30587.15)
+v3=Voiture("BMW", "M3", 2025, 89550.30)
+v4=Voiture("Audi", "RS6", 2025, 100020.54)
+
+ajouter_voiture(v1)
+ajouter_voiture(v2)
+ajouter_voiture(v3)
+ajouter_voiture(v4)
