0cf575f38f071266dc5dafc24bf5aebfed228414
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-06 01:45:05 -0400

    Création du crud_db.py et création de la fonction connecter_db

---
+++ crud_db.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import json
+
+def connecter_db():
+    with open ("config.json") as f:
+        config=json.load(f)
+
+    connexion=mysql.connector.connect(
+        host=config["host"],
+        user=config["user"],
+        passwor=config["password"],
+        database=config["database"]
+    )
+    return connexion
