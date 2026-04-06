070e9ad4c72d4748c614755bc4011405b6eb2588
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-06 01:51:34 -0400

    Test de connexion à MySQL

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from Voiture import Voiture
 v1=Voiture("Volkswagen", "Jetta", "2024", 29478)
 v1.afficher_info()
+
+from crud_db import connecter_db
+try:
+    connexion = connecter_db()       # Tente de se connecter
+    print("Connexion réussie !") # Si pas d'erreur, affichage
+except Exception as e:
+    print("Erreur de connexion :", e)  # Affiche l'erreur si ça ne marche pas
+finally:
+    # Vérifie si la connexion existe et est ouverte avant de fermer
+    if 'conn' in locals() and connexion.is_connected():
+        connexion.close()
