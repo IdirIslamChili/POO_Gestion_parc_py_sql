c048f8c014cbafba54f082a24b10f4c513e01fdd
Idir Islam Chili <chiliidirislam@gmail.com>
2026-04-06 13:09:25 -0400

    Correction du nom de la méthode 'afficher_voiture' et définition de la connexion=none avant le bloc try except

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from Voiture import Voiture
-v1=Voiture("Volkswagen", "Jetta", "2024", 29478)
-v1.afficher_info()
+from voiture import Voiture
+
+v1 = Voiture("Volkswagen", "Jetta", 2024, 29478)
+v1.afficher_voiture()
 
 from crud_db import connecter_db
+
+connexion = None
+
 try:
     connexion = connecter_db()
     print("Connexion réussie !")
 except Exception as e:
     print("Erreur de connexion :", e)
 finally:
-    if 'conn' in locals() and connexion.is_connected():
+    if 'connexion' in locals():
         connexion.close()
